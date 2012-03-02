e16647a5272e0397faa4303b6586b21de410774f
Tony Zhang <shufan.tsz@gmail.com>
2012-03-02 02:19:01 -0500

    added RMI classes from project 1

---
+++ project2/rmi/MethodCall.java
@@ -0,0 +1,18 @@
+package project2.rmi;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+
+public class MethodCall implements Serializable
+{
+	String name;
+	Class[] types;
+	Object[] params;
+	
+	public MethodCall(Method method, Object[] params) {
+		name = method.getName();
+		this.params = params;
+		types = method.getParameterTypes();
+	}
+	
+}
